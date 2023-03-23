e1e50ed12cd3dfab8b0379ae422225145962d215
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-23 15:18:16 +0100

    Updated background image for App component

--- tesla-clone/src/App.jsx
+++ tesla-clone/src/App.jsx
@@ -1,7 +1,8 @@
-function App() {
 
+
+function App() {
   return (
-    <div className="text-3xl font-bold underline">
+    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center">
       <h1>Tesla Clone</h1>
     </div>
   )
