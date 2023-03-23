9283aa5e1cbe5bb61caa26c94a4588c4c2b99a06
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-23 16:32:51 +0100

    Create & Add Navbar component

--- tesla-clone/src/App.jsx
+++ tesla-clone/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center">
-      <h1>Tesla Clone</h1>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tesla-clone/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img />
+      </div>
+      <div>
+        <ul>
+          <li>Model S</li>
+          <li>Model 3</li>
+          <li>Model X</li>
+          <li>Model Y</li>
+          <li>Solar Roof</li>
+          <li>Solar Panels</li>
+        </ul>
+      </div>
+      <div>
+      <ul>
+          <li>Shop</li>
+          <li>Account</li>
+          <li>Menu</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
