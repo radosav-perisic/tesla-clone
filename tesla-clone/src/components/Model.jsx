e30352c0a8918e9a5c4d0bb5bd12c670077fdf77
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-24 10:25:17 +0100

    Add styling to Custom Order button

--- tesla-clone/src/components/Model.jsx
+++ tesla-clone/src/components/Model.jsx
@@ -9,7 +9,7 @@ const Model = () => {
             <p className='p-2 text-sm'>Order Online for <span className='underline underline-offset-4'>Touchless Delivery</span> </p>
         </div>
         <div className='flex flex-col items-center justify-center text-sm lg:flex-row absoulte inset-x-0 bottom-[10%]'>
-            <button>Custom Order</button>
+            <button className='bg-[#393c41] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white '>Custom Order</button>
             <button>Existing Inventory</button>
         </div>
         <div>
