205eb469456e6722af63963a65354d4164480234
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-24 16:29:51 +0100

    Update Model3 with Leasing and Demo Drive options

--- tesla-clone/src/components/Model3.jsx
+++ tesla-clone/src/components/Model3.jsx
@@ -6,11 +6,11 @@ const Model3 = () => {
     <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center">
         <div className='flex-col pt-24 inset-x-0 top-[15%] text-center'>
             <h1 className='text-4xl font-bold'>Model 3</h1>
-            <p className='p-2 text-sm'>Order Online for <span className='underline underline-offset-4'>Touchless Delivery</span> </p>
+            <p className='p-2 text-lg'>Leasing starting at $349/mo</p>
         </div>
         <div className='flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]'>
-            <button className='bg-[#393c41] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white '>Custom Order</button>
-            <button className='bg-[#f4f4f4] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 '>Existing Inventory</button>
+            <button className='bg-[#393c41] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white'>Custom Order</button>
+            <button className='bg-[#f4f4f4] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-gray-700 font-bold'>Demo Drive</button>
         </div>
         <div className='flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]'>
             <FiChevronDown size={32}/>
