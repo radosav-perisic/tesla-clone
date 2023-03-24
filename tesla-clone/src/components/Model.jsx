2cd34fbd1141e2925512d9b9c2bebb78a432c653
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-24 10:28:51 +0100

    Update downward arrow icon position and animation

--- tesla-clone/src/components/Model.jsx
+++ tesla-clone/src/components/Model.jsx
@@ -8,12 +8,12 @@ const Model = () => {
             <h1 className='text-4xl font-bold'>Model 3</h1>
             <p className='p-2 text-sm'>Order Online for <span className='underline underline-offset-4'>Touchless Delivery</span> </p>
         </div>
-        <div className='flex flex-col items-center justify-center text-sm lg:flex-row absoulte inset-x-0 bottom-[10%]'>
+        <div className='flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]'>
             <button className='bg-[#393c41] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white '>Custom Order</button>
             <button className='bg-[#f4f4f4] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 '>Existing Inventory</button>
         </div>
-        <div>
-            <FiChevronDown />
+        <div className='flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]'>
+            <FiChevronDown size={32}/>
         </div>
     </div>
   )
