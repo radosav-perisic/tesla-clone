1690928a9264ed3166ba8f4270eba2bda14d5f42
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-24 10:12:49 +0100

    Improve Model component layout and styles

--- tesla-clone/src/components/Model.jsx
+++ tesla-clone/src/components/Model.jsx
@@ -4,8 +4,8 @@ import {FiChevronDown} from 'react-icons/fi'
 const Model = () => {
   return (
     <div>
-        <div>
-            <h1>Model 3</h1>
+        <div className='absolute inset-x-0 top-[15%] text-center'>
+            <h1 className='text-4xl font-bold'>Model 3</h1>
             <p>Order Online for Touchless Delivery</p>
         </div>
         <div>
