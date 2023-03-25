08d9437d62ff8ad88f2f297df01b6832e4366550
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-25 15:32:23 +0100

    Add SolarPanels component to page

--- tesla-clone/src/App.jsx
+++ tesla-clone/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <ModelY />
       <ModelS />
       <ModelX />
-      
+      <SolarPanels />
     </div>
   );
 }
---
+++ tesla-clone/src/components/SolarPanels.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const SolarPanels = () => {
+  return (
+    <div>
+      <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/da02ce3e-3ede-4fa4-8fcb-e8d5d6681cfb/bvlatuR/std/600x1576/Homepage-SolarPanels-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/16b04537-a4be-4bf9-8637-86862a858da8/bvlatuR/std/2880x1800/_25-HP-SolarPanels-D')] h-screen bg-cover bg-center relative">
+        <div className="flex-col pt-24 inset-x-0 top-[15%] text-center">
+          <h1 className="text-4xl font-bold">Solar Panels</h1>
+        </div>
+        <div className="flex-col absolute z-40 flex items-center justify-center text-sm lg:flex-row inset-x-0 bottom-[10%]">
+          <a href="https://www.tesla.com/model3/design#overview">
+            {" "}
+            <button className="bg-[#393c41] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white">
+              Custom Order
+            </button>
+          </a>
+          <a href="https://www.tesla.com/drive">
+            {" "}
+            <button className="bg-[#f4f4f4] rounded-mb w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-gray-700 font-bold">
+              View Inventory
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SolarPanels
