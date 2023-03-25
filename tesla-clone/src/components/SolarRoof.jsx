9ff293b79830f4e3e2882978d290156446a24545
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-25 16:25:10 +0100

    Update SolarRoof styling

--- tesla-clone/src/components/SolarRoof.jsx
+++ tesla-clone/src/components/SolarRoof.jsx
@@ -4,7 +4,7 @@ const SolarRoof = () => {
   return (
     <div>
       <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/3f6f20d2-ad05-41b7-b181-9fb6f01af119/bvlatuR/std/800x2100/Homepage-SolarRoof-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/4a41ec0e-7316-46a8-8ce9-e2f195ba644d/bvlatuR/std/2880x1800/Homepage-SolarRoof-Desktop')] h-screen bg-cover bg-center relative">
-        <div className="flex-col pt-24 inset-x-0 top-[15%] text-center">
+        <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
           <h1 className="text-4xl font-bold text-gray-900">Solar Roof</h1>
         </div>
         <div className="flex-col absolute z-40 flex items-center justify-center text-sm lg:flex-row inset-x-0 bottom-[10%]">
