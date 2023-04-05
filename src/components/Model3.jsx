42a3df9d1b374192613413e4703bc0213bf4bd2f
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-05 13:04:25 +0200

    Refactor Model3 component layout

--- src/components/Model3.jsx
+++ src/components/Model3.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { FiChevronDown } from "react-icons/fi";
+
 
 const Model3 = () => {
   return (
-    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center snap-start">
+    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center relative snap-start">
       <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
         <h1 className="text-4xl font-bold text-gray-900">Model 3</h1>
         <p className="p-2 text-lg text-gray-800/90">Leasing starting at $349/mo</p>
@@ -21,9 +21,6 @@ const Model3 = () => {
             Demo Drive
           </button>
         </a>
-      </div>
-      <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
-        <FiChevronDown size={32} />
       </div>
     </div>
   );
