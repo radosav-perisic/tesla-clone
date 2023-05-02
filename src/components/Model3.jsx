f36052d4f3849879f82113a646d941a48eb9a0cd
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-02 23:09:01 +0200

    Update Model3 component with latest changes
    Update Model3 component of the web app to reflect latest changes on Tesla webpage

--- src/components/Model3.jsx
+++ src/components/Model3.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const Model3 = () => {
   return (
-    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center relative snap-start">
+    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen lg bg-cover bg-center relative snap-start">
       <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
         <h1 className="text-4xl font-bold text-gray-900">Model 3</h1>
-        <p className="p-2 text-lg text-gray-800/90">Leasing starting at $349/mo</p>
+        <p className="p-2 pt-3 text-gray-900/90 underline text-sm">View Inventory</p>
       </div>
       <div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]">
         <a href="https://www.tesla.com/model3/design#overview">
           {" "}
-          <button className=" lg:mx-4 h-10 mt-2 rounded-[0.250rem] bg-[#1c2025]/80 text-gray-50 tracking-wider w-72 lg:w-[16.5rem] mb-2 font-semibold">
+          <button className=" lg:mx-4 h-10 mt-2 rounded-[0.250rem] bg-[#1c2025]/80 text-gray-50 tracking-wider w-72 lg:w-[16.5rem] font-semibold">
            Order Now
           </button>
         </a>
