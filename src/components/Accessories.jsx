b92fe40c793dfde9a5d088e049cf802abe3262bb
Radosav Perisic <radosavperisic00@gmail.com>
2024-01-15 17:47:44 +0100

    Update Accessories
    Replace Deprecated Image URLs

--- src/components/Accessories.jsx
+++ src/components/Accessories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Accessories = () => {
   return (
     <div>
-      <div className="snap-start bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/12f15753-d4af-4f4a-920b-65e9e260c521/bvlatuR/std/800x2100/Homepage-Accessories-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/dd739764-bcaa-4263-9488-8c73bc9fb046/bvlatuR/std/0x0/dd739764-bcaa-4263-9488-8c73bc9fb046')] h-screen bg-cover bg-center relative">
+      <div className="snap-start bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Accessories-Mobile-NA-APAC.jpg')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Accessories-Desktop-NA-APAC.jpg')] h-screen bg-cover bg-center relative">
         <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
           <h1 className="text-4xl font-bold text-gray-900">Accessories</h1>
         </div>
