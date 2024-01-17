f84058ae2fb1668419925f0791a8ab5d516d2a12
Radosav Perisic <radosavperisic00@gmail.com>
2024-01-17 14:42:53 +0100

    Upload new modelY image & update ModelY.jsx
    Improved the styling and content of the title and its description, ensuring a more visually appealing presentation

--- src/components/ModelY.jsx
+++ src/components/ModelY.jsx
@@ -6,8 +6,8 @@ const ModelY = () => {
   return (
     <div id='modelY'  style={{'--image-url': `url(${teslaY})`}}   className="snap-start bg-[image:var(--image-url)] h-screen bg-cover bg-center relative">
       <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
-        <h1 className="text-4xl font-bold text-gray-900">Model Y</h1>
-        <p className="p-2 pt-3 text-gray-900/90 underline text-sm">View Inventory</p>
+        <h1 className="text-[2.5rem] font-bold text-gray-900">Model Y</h1>
+        <p className="text-gray-900/90 font-normal text-xl">Lease starting at $399/mo*</p>
       </div>
       <div className="flex-col absolute z-40 flex items-center justify-center text-sm lg:flex-row inset-x-0 bottom-[10%]">
         <a href="https://www.tesla.com/modely/design#overview">
