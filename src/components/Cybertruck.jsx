51f941b064475c0eb1457f87250bfc9f1dac835c
Radosav Perisic <radosavperisic00@gmail.com>
2024-01-21 11:10:32 +0100

    Finalize Cybertruck.jsx setup

--- src/components/Cybertruck.jsx
+++ src/components/Cybertruck.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Cybertruck = () => {
   return (
-    <div>Cybertruck</div>
-  )
-}
+    <div>
+      <div className="bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Cybertruck-Mobile.jpg')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Cybertruck-Desktop.jpg')] h-screen bg-cover bg-center relative snap-start">
+        <div className="flex-col absolute z-40 flex items-center justify-center text-sm lg:flex-row inset-x-0 bottom-[10%]">
+          <a href="https://www.tesla.com/cybertruck/design#overview">
+            <button className="rounded-[0.250rem] bg-[#1c2025]/80 text-gray-50 tracking-wider w-72 lg:w-[16.5rem] font-semibold lg:mx-4 h-10 mt-2">
+            Order Now
+          </button>
+        </a>
+        <a href="https://www.tesla.com/drive">
+          <button className="bg-[#f4f4f4]/50 rounded-[0.250rem] w-72 lg:w-[16.5rem] lg:mx-4 h-10 mt-2 text-gray-700/90 font-bold">
+            Demo Drive
+          </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Cybertruck
+export default Cybertruck;
