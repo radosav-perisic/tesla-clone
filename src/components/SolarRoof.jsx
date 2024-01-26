4ddce8b7a68f74172342a3134fc088009d08b13c
Radosav Perisic <radosavperisic00@gmail.com>
2024-01-26 14:54:34 +0100

    Update SolarRoof.jsx

--- src/components/SolarRoof.jsx
+++ src/components/SolarRoof.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SolarRoof = () => {
   return (
     <div>
-      <div className=" snap-start bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/3f6f20d2-ad05-41b7-b181-9fb6f01af119/bvlatuR/std/800x2100/Homepage-SolarRoof-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/4a41ec0e-7316-46a8-8ce9-e2f195ba644d/bvlatuR/std/2880x1800/Homepage-SolarRoof-Desktop')] h-screen bg-cover bg-center relative">
+      <div className=" snap-start bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/h_2100,w_800,c_fit,f_auto,q_auto:best/Homepage-SolarRoof-Mobile')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Homepage-SolarRoof-Desktop-Global')] h-screen bg-cover bg-center relative">
         <div className="flex-col pt-32 inset-x-0 top-[15%] text-center">
           <h1 className="text-4xl font-bold text-gray-900">Solar Roof</h1>
           <p className="p-2 pt-3 text-gray-900/90 text-sm">Produce Clean Energy From Your Roof</p>
